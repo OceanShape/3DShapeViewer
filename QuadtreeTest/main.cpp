a8aab2b4f6965e4844dbe8c3e370c52d2e05cc1f
OceanShape <oceanshape700@gmail.com>
2023-04-10 16:01:50 +0900

    feat: Add side checking code

--- QuadtreeTest/main.cpp
+++ QuadtreeTest/main.cpp
@@ -6,21 +6,57 @@
 
 using namespace std;
 
-struct Quadtree {
+struct QuadtreeNode {
 	vector<int> objects;
-	vector<int> objectVertexCount;
+	vector<int> objectVertexCounts;
 
 	float Xmin, Xmax, Ymin, Ymax;
-	shared_ptr<Quadtree> nodes[4] = { nullptr, nullptr, nullptr, nullptr };
+	float Xmid, Ymid;
+	shared_ptr<QuadtreeNode> nodes[4] = { nullptr, nullptr, nullptr, nullptr };
 
-	Quadtree(float Xmin, float Xmax, float Ymin, float Ymax) : Xmin(Xmin), Xmax(Xmax), Ymin(Ymin), Ymax(Ymax) {
+	QuadtreeNode(const float& _Xmin, const float& _Xmax, const float& _Ymin, const float& _Ymax) : Xmin(_Xmin), Xmax(_Xmax), Ymin(_Ymin), Ymax(_Ymax), Xmid((_Xmin + _Xmax) / 2), Ymid((_Ymin + _Ymax) / 2) {
 	}
 
-	void store(const vector<int>& vertices) {
-
+	bool isLeft(const float& _Xmin, const float& _Xmax) {
+		return Xmin <= _Xmin && Xmin <= _Xmax && _Xmin <= Xmid && _Xmax <= Xmid;
 	}
 
-	void render() {
+	bool isRight(const float& _Xmin, const float& _Xmax) {
+		return Xmid <= _Xmin && Xmid <= _Xmax && _Xmin <= Xmax && _Xmax <= Xmax;
+	}
+
+	bool isUp(const float& _Ymin, const float& _Ymax) {
+		return Ymid <= _Ymin && Ymid <= _Ymax && _Ymin <= Ymax && _Ymax <= Ymax;
+	}
+
+	bool isDown(const float& _Ymin, const float& _Ymax) {
+		return Ymin <= _Ymin && Ymin <= _Ymax && _Ymin <= Ymid && _Ymax <= Ymid;
+	}
+
+	void store(const vector<int>& objectVertices, const float& _Xmin, const float& _Xmax, const float& _Ymin, const float& _Ymax) {
+		if (isLeft(_Xmin, _Xmax) && isUp(_Ymin, _Ymax)) {
+			nodes[0] = make_shared<QuadtreeNode>(Xmin, Xmid, Ymid, Ymax);
+			nodes[0]->store(objectVertices, _Xmin, _Xmax, _Ymin, _Ymax);
+		}
+		else if (isRight(_Xmin, _Xmax) && isUp(_Ymin, _Ymax)) {
+			nodes[1] = make_shared<QuadtreeNode>(Xmid, Xmax, Ymid, Ymax);
+			nodes[1]->store(objectVertices, _Xmin, _Xmax, _Ymin, _Ymax);
+		}
+		else if (isLeft(_Xmin, _Xmax) && isDown(_Ymin, _Ymax)) {
+			nodes[2] = make_shared<QuadtreeNode>(Xmin, Xmid, Ymin, Ymid);
+			nodes[2]->store(objectVertices, _Xmin, _Xmax, _Ymin, _Ymax);
+		}
+		else if (isRight(_Xmin, _Xmax) && isDown(_Ymin, _Ymax)) {
+			nodes[3] = make_shared<QuadtreeNode>(Xmid, Xmax, Ymin, Ymid);
+			nodes[3]->store(objectVertices, _Xmin, _Xmax, _Ymin, _Ymax);
+		}
+		else {
+			objects.insert(objects.end(), objectVertices.begin(), objectVertices.end());
+			objectVertexCounts.push_back(objectVertices.size());
+		}
+	}
+
+	void render(int level) {
 		bool allNodeNull;
 		for (auto n : nodes) {
 
@@ -36,12 +72,12 @@ struct ObjectData {
 		root = make_shared<qtNode>(Xmin, Xmax, Ymin, Ymax);
 	}
 
-	void store(const vector<int>& vertices) {
-		root->store(vertices);
+	void store(const vector<int>& allObjectVertices) {
+		//root->store(allObjectVertices);
 	}
 
-	void render() {
-		root->render();
+	void render(int level) {
+		//root->render();
 	}
 };
 
@@ -49,7 +85,13 @@ struct ObjectData {
 
 
 int main() {
-	cout << "Hi" << endl;
+	std::vector<int> source{ 1, 2, 3, 4, 5 };
+	std::vector<int> dest{ 9, 8, 7, 6 };
+
+	
+	for (auto i : dest) {
+		cout << i << " ";
+	}
 
 	return 0;
 }
