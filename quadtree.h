ebafe4a8596ad5cb1520c0f5670008164d448649
OceanShape <oceanshape700@gmail.com>
2023-04-19 15:09:44 +0900

    fix: Revise vertex type(int to float)

--- quadtree.h
+++ quadtree.h
@@ -8,7 +8,7 @@
 using namespace std;
 
 struct QuadtreeNode {
-	vector<int> objectVertices;
+	vector<float> objectVertices;
 	vector<int> objectVertexCounts;
 
 	float Xmin, Xmax, Ymin, Ymax, Zmin, Zmax;
