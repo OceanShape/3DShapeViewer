30aa490d198d71d7531f751ec12a3f804397648c
OceanShape <oceanshape700@gmail.com>
2023-04-21 15:22:15 +0900

    refactor: Remove memory allocation in Object.render()

--- object.h
+++ object.h
@@ -5,9 +5,9 @@
 #include <GLES3/gl3.h>
 
 struct Vertex {
-	double x;
-	double y;
-	double z;
+	float x;
+	float y;
+	float z;
 };
 
 struct SHPPoint {
@@ -22,19 +22,19 @@ private:
 	int vertexCount = 0;
 
 public:
-	//float min[3]{ DBL_MAX, DBL_MAX, DBL_MAX };
-	//float max[3]{ DBL_MIN, DBL_MIN, DBL_MIN };
-	double min[3]{ DBL_MAX, DBL_MAX, DBL_MAX };
-	double max[3]{ DBL_MIN, DBL_MIN, DBL_MIN };
+	float min[3]{ FLT_MAX, FLT_MAX, FLT_MAX };
+	float max[3]{ FLT_MIN, FLT_MIN, FLT_MIN };
+	//double min[3]{ DBL_MAX, DBL_MAX, DBL_MAX };
+	//double max[3]{ DBL_MIN, DBL_MIN, DBL_MIN };
 
 	Object(int _vertexCount, SHPPoint* _vertices) : vertexCount(_vertexCount) {
 		vertices = new Vertex[vertexCount];
 		indices = new GLuint[vertexCount];
 
 		for (size_t i = 0; i < vertexCount; ++i) {
-			double x = _vertices[i].x;
-			double y = _vertices[i].y;
-			double z = 0.0f;
+			float x = _vertices[i].x;
+			float y = _vertices[i].y;
+			float z = 0.0f;
 
 			min[0] = std::min(min[0], x);
 			max[0] = std::max(max[0], x);
@@ -55,17 +55,8 @@ public:
 	}
 
 	void render() {
-		auto renderVertices = new float[vertexCount * 3];
-		for (int i = 0; i < vertexCount; ++i) {
-			renderVertices[i * 3] = vertices[i].x;
-			renderVertices[i * 3 + 1] = vertices[i].y;
-			renderVertices[i * 3 + 2] = 0.0f;
-		}
-
-		glBufferData(GL_ARRAY_BUFFER, vertexCount * 3 * sizeof(float), renderVertices, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, vertexCount * 3 * sizeof(float), vertices, GL_STATIC_DRAW);
 		glDrawArrays(GL_LINE_STRIP, 0, vertexCount);
-
-		delete[] renderVertices;
 	}
 
 	~Object() {
