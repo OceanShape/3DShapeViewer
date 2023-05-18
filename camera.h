49a20b0c0043fd66c6f518e20bb50e68a4118724
OceanShape <oceanshape700@gmail.com>
2023-05-18 16:03:51 +0900

    feat: Add updateKey(replace later)

--- camera.h
+++ camera.h
@@ -9,21 +9,25 @@ using namespace glm;
 class Camera {
 
 public:
+	Camera(float posX, float posY, float posZ) {
+		position = vec3(posX, posY, posZ);
+	}
+
 	mat4 getView() { return lookAt(position, position + direction, up); };
 	mat4 getProj() { return perspective(radians(projFov), aspect, nearZ, farZ); };
 	vec3 getEyePos() { return position; };
 
 	void updateMouse(float mouseNdcX, float mouseNdcY);
-	void rotateKey(float rotX, float rotY, float rotZ);
+	void updateKey(float rotX, float rotY, float rotZ);
 	void moveForward(float dt) { position += direction * delta * dt; };
 	void moveRight(float dt) { position += right * delta * dt; };
 	void setAspectRatio(float _aspect) { aspect = _aspect; };
 
 private:
-	glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f);
-	glm::vec3 direction = glm::vec3(0.0f, 0.0f, -1.0f);
-	glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
-	glm::vec3 right = glm::vec3(1.0f, .0f, .0f);
+	vec3 position = vec3(0.0f, 0.0f, 3.0f);
+	vec3 direction = vec3(0.0f, 0.0f, -1.0f);
+	vec3 up = vec3(0.0f, 1.0f, 0.0f);
+	vec3 right = vec3(1.0f, .0f, .0f);
 
 	float pitch = .0f;
 	float yaw = .0f;
@@ -42,6 +46,6 @@ void Camera::updateMouse(float mouseNdcX, float mouseNdcY) {
 
 }
 
-void Camera::rotateKey(float rotX, float rotY, float rotZ) {
+void Camera::updateKey(float rotX, float rotY, float rotZ) {
 
 }
