dcbaff73e7a9f30ae6d742ecba068cb914bd3e3f
OceanShape <oceanshape700@gmail.com>
2023-05-19 15:47:28 +0900

    feat: Create ShapeViewer class

--- 3DShapeViewer.h
+++ 3DShapeViewer.h
@@ -26,13 +26,58 @@
 
 using namespace std;
 
+class ShapeViewer {
+public:
+	LRESULT msgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		int wDel;
+		switch (message)
+		{
+		case WM_COMMAND:
+		{
+			int wmId = LOWORD(wParam);
+			switch (wmId)
+			{
+			case IDM_OPEN:
+				openShapefile();
+				break;
+			case IDM_EXIT:
+				DestroyWindow(hWnd);
+				break;
+			default:
+				return DefWindowProc(hWnd, message, wParam, lParam);
+			}
+		}
+		break;
+		//case WM_MOUSEWHEEL:
+		//	wDel = GET_WHEEL_DELTA_WPARAM(wParam) / 120;
+		//	fov -= wDel * .1f;
+		//	fov = (fov > 89.0f) ? 89.0f : (fov < 5.0f) ? 5.0f : fov;
+		//	std::cout << "fov: " << fov << endl;
+		//	break;
+		case WM_KEYDOWN:
+			keyPressed[wParam] = true;
+			break;
+		case WM_KEYUP:
+			keyPressed[wParam] = false;
+			break;
+		case WM_DESTROY:
+			closeShapefile();
+			::PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		return 0;
+	}
+};
+
 bool checkShaderCompileStatus(GLuint shader);
 bool compileShader(GLuint shader, const char* source);
 
 bool initialize();
 void render();
 void cleanUp();
-void setLevelAndViewBoundary();
 
 string readShader(const string& filepath);
 bool openShapefile();
